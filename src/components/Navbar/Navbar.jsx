ad6a47c068556b9a585464620e1d057388c6f972
bibungthi053 <bibungthi1230@gmail.com>
2024-10-12 19:26:45 +0530

    Refactor responsive styles for mobile devices

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,30 @@
-import './Navbar.css'
+import { useState } from 'react';
+import './Navbar.css';
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar-container">
-        <div className="logo"><a href="./">ChatLinker</a></div>
-        <ul className="navlinks">
-            <li><a href="./">Home</a></li>
-            <li><a href="#About">About</a></li>
-            <li><a href="#Contact">Contact</a></li>
-        </ul>
-        <div className="navbar-buttons">
-            <button className='get-in-touch'>Get In Touch</button>
-            <button className='start-chatting'>Start Chatting</button>
-        </div>
+      <div className="logo"><a href="./">ChatLinker</a></div>
+      <button className="navbar-toggle" onClick={toggleNavbar}>
+        ☰
+      </button>
+      <ul className={`navlinks ${isOpen ? 'open' : ''}`}>
+        <li><a href="./">Home</a></li>
+        <li><a href="#About">About</a></li>
+        <li><a href="#Contact">Contact</a></li>
+      </ul>
+      <div className={`navbar-buttons ${isOpen ? 'open' : ''}`}>
+        <button className='get-in-touch'>Get In Touch</button>
+        <button className='start-chatting'>Start Chatting</button>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
